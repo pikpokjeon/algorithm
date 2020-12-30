df3addb89d252550d549e0283fc64a04558e09d1
yaejinjeon <pikpokjeon@ipactory.com>
2020-12-30 18:23:44 +0900

    알고 백준 첫날 마스터

---
+++ 백준/ab.js
@@ -0,0 +1,18 @@
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+// 출력
+// 첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
+
+const fs = require('fs')
+
+var input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+  .map(function(a) {
+    return +a
+  })
