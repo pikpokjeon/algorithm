72246870307e52ce65318c32ddf041a4a7bfa758
pikpokjeon <pikpokjeon@gmail.com>
2021-01-17 23:51:04 +0900

    가변적으로 증가하는 배열의 길이를 해당 배열에 map으로 돌면서 캐치 할 수 있는지

--- random/ss.js
+++ random/ss.js
@@ -17,3 +17,6 @@ function solution(strings, n) {
 console.log(solution(["abce", "abcd", "cdx"], 2))
 
 //	[abcd, abce, cdx]
+
+let a =[1,2,3,4,5,6]
+a.map(e => {a.push(e+1); for(i=0; i<a.length; i++){console.log('a:',a,i,a.length,a[a.length-1])}})
