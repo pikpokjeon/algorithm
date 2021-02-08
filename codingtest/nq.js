56e71d21d204acdfa0cf41c972931821a9d38cde
pikpokjeon <pikpokjeon@gmail.com>
2021-02-08 21:52:46 +0900

    working on recurisve func for nqueen

--- codingtest/nq.js
+++ codingtest/nq.js
@@ -1,74 +1,77 @@
 
 
-let M = 4
+let M = 5
 
 //앤퀸이 놓인자리
-let visit = Array(M+1).fill(-1)
+let visit = Array(M).fill(-1)
 
 
 
-const visitArry = (arr, i,n) =>
+// const visitArry = (arr, i,n) =>
+// {
+//     if( n+1 <= M && n-1 > 0)
+//     {
+//         arr[n] = n
+//         arr[Math.min(M,n+i)] = Math.min(M,n+i)
+//         arr[Math.min(M,n+i-1)] = -1
+//         arr[Math.max(1,n-i)] = Math.max(1,n-i)
+//         arr[Math.max(1,n-i+1)] = -1
+//         console.log('\n')
+//         console.log(n,'번에 퀸이 있으면-----',i,'------')
+//         console.table(arr)
+
+//     }
+//     return arr
+// }
+let count = 0
+const go = (i,round,memo,nq,arr) =>
 {
-    if( n+1 <= M && n-1 > 0)
+    // console.log('i: ',i,'round: ',round,'nq: ',nq,'arr: ',arr)
+    // if(round === M - 1) return
+    if(arr.length === M )
     {
-        arr[n] = n
-        arr[Math.min(M,n+i)] = Math.min(M,n+i)
-        arr[Math.min(M,n+i-1)] = -1
-        arr[Math.max(1,n-i)] = Math.max(1,n-i)
-        arr[Math.max(1,n-i+1)] = -1
-        console.log('\n')
-        console.log(n,'번에 퀸이 있으면-----',i,'------')
-        console.table(arr)
-
-    }
-    return arr
-}
-let k = 0
-const go = (n,visit,arr,count) =>
-{
-    // console.log(n)
-    if(arr.length >= M)
-    {
-        count +=1
-
-        // console.log(arr,'end',count)
-        
+        count += 1
+        console.log(arr)
         return
     }
-    
-    let check = Array(M+1).fill(-1)
-    for(let k = 1; k <= M; k++)
+    let init = Array(M).fill(-1)
+    if(nq[i] < 0 && memo[i] < 0 && arr.indexOf(i) < 0)
     {
-
-        let v = visitArry(check,k,n+1)
-        visit = v
-        if(visit[k] < 0)
+        nq[i] = 1, arr.push(i)
+        for(let j = 0; j < arr.length; j ++)
         {
-            console.log(n,count,'Possible Number ------:', k)
-            if(v[k] < 0)
+            const [a,b] = [arr[j]-round, arr[j]+round]
+            if(a > -1)memo[a] = 1
+            if(b < M)memo[b] = 1
+            // console.log('a: ',a,'b: ',b,'arr[j]:', arr[j],'round: ',round,'memo: ',memo)
+            for(let k = 0; k < M; k++)
             {
-                console.log('Entered!')
-                
-                visit[k] = v[k]
-                arr.push(k)
-                // console.log('arr:',arr)
-
-                visitArry(v,k,n+1)
-                visit = v
-                go(n+1,v,arr,count)
+                if(arr.indexOf(k) < 0 && memo[k] < 0 && nq[k]< 0)
+                {
+                    arr.push(k)
+                    // console.log('i :',i,'k :',k)
+                    // console.log(arr)
+                    console.log('--------------------')
+                    return go(i+1, round+1, init , nq, arr)
+                }
             }
         }
     }
-    // console.table(arr)
     return arr
 }
 
 // 시작지점을 넣는다 (첫번째 퀸의 자리)
 
-for( let i = 1; i <= M; i++)
-{
-    go(i,visit,[],0)
+for( let i = 0; i < M; i++)
+{ 
+    let memo = Array(M).fill(-1)
+    let nq = Array(M).fill(-1)
+    // 현재인덱스, 순회횟수, 매번체크방문, 
+    go(i,0,memo,nq,[])
+    continue
 }
+console.log(count)
+
 
 // 해당지점에서 놓일 수 없는 곳을 표시한다 (제한배열)
 
@@ -83,95 +86,3 @@ for( let i = 1; i <= M; i++)
 // 첫째 앤퀸 자리를 옮겨서 다시 탐색
 
 
-
-
-
-// const go = (count,arr,visit) =>
-// {
-//     if(arr.length === n )
-//     {
-//         console.log(arr,'!!!')
-//         return
-//     }
-//     for(let i = 0; i < n; i ++)
-//     {
-//         console.log(visit)
-//         // let flag = arr.indexOf(i) < 0
-//         // if(visit[i] < 0 )
-//         // {
-//             console.log(i)
-//             if(visit.indexOf(i)< 0)
-//             {   visit[i] = i
-//                 visit[i+1] = i+1
-//                 visit[Math.max(0,i-1)] = i-1
-//                 arr.push(i)
-//                 go(i+1,arr,visit)
-//                 arr.pop()
-//             }
-//             // visit[i] = -1
-
-            
-
-//         // }
-//     }
-// }
-
-
-// go(0,[],visit)
-
-// const go = (a ,now, arr,visit) =>
-// {
-//     console.log(arr)
-//     if(arr.length === 30) 
-//     {
-//         console.log(arr,'-----done')
-
-//     }
-//     arr.push(now)
-
-//     arr.push(Math.max(1,now-1))
-    
-//     arr.push(now+1)
-//     for(let i = 1; i <= n+1; i++)
-//     {
-//         if(visit[i] < 0)
-//         {
-//             console.log(i)
-//             visit[i] = i
-//             go(a,i,arr,visit)
-//         }
-//     }
-
-// }
-// const a = (num,arr) =>
-// {
-//     if(arr.length > 10 )return
-//     for(let i = 1; i <= n; i++ )
-//     {
-//         if (arr.indexOf(i) < 0)
-//         {
-//             arr.push(i)
-//             v(arr,i)
-
-//         }
-//     }
-//     console.log(num,'result', arr)
-//     return arr
-// }
-
-// const v = (box,num) =>
-// {
-//     if( num > n) return
-//     box.push(num+1)
-//     box.push(Math.max(1,num-1))
-    
-//     return box
-// }
-// let box = [4,1,5]
-
-
-
-// for(let i = 0; i< box.length; i++){
-//     a(box[i],box)
-// }
-// go(0,4,[],visit)
