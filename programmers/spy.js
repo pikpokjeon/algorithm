ff18fbb5c3972d1dc283fc2e742d7f8303cf12db
personal <pikpok@yaejinui-MacBookPro.local>
2021-02-26 22:55:39 +0900

    각 항목에 맞는 아이템 추가

--- programmers/spy.js
+++ programmers/spy.js
@@ -1,14 +1,26 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42578
 function solution(clothes) {
     var answer = 0;
-    let all = new Set()
+    let a = {}
+    // let all = new Set()
     clothes.forEach( e =>
-        e.forEahc( a => all.add(a))
+       {
+        if(!a[e[1]])
+        {
+            a[e[1]] = []
+        }
+        a[e[1]].push(e[0])
+       } 
     )
     
-    console.log(all)
+    console.log(a)
     
     
     
     return answer;
 }
+
+const input = [[["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]], 
+                [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]]
+
+input.forEach( e => solution(e))
