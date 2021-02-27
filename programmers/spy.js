24849d00da1b36e35c31c307f9a4cd90f6eb04c4
personal <pikpok@yaejinui-MacBookPro.local>
2021-02-27 15:36:30 +0900

    create memo array

--- programmers/spy.js
+++ programmers/spy.js
@@ -1,6 +1,6 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42578
 function solution(clothes) {
-    var answer = 0;
+    let count = 0
     let a = {}
     // let all = new Set()
     clothes.forEach( e =>
@@ -10,10 +10,24 @@ function solution(clothes) {
             a[e[1]] = []
         }
         a[e[1]].push(e[0])
+        count += 1
        } 
     )
     
-    console.log(a)
+    let answer = []
+    for(const [key,val] of Object.entries(a))
+    {
+        // console.log(key,val)
+
+    }
+    let memo = Object.entries(a).map(e => e[1].map( c => -1))
+    console.log(memo)
+    const jegui = (arr,i) =>
+    {
+
+    }
+    
+    // console.log(answer)
     
     
     
