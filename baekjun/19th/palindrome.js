7d2974bef5b029019e808b67d4df6e4a13b00251
pikpokjeon <pikpokjeon@gmail.com>
2021-03-29 00:44:52 +0900

    맞았는데 왜 틀려 펠린드룸 골드 2

--- baekjun/19th/palindrome.js
+++ baekjun/19th/palindrome.js
@@ -1,6 +1,7 @@
 // 문제 https://www.acmicpc.net/problem/1053
 // 팰린드롬이란, 앞에서부터 읽었을 때와, 뒤에서부터 읽었을 때가 같은 문자열이다.
 
+
 // 모든 문자열이 팰린드롬이 아니기 때문에 다음과 같은 4가지 연산으로 보통 문자열을 팰린드롬으로 만든다.
 
 // 문자열의 어떤 위치에 어떤 문자를 삽입 (시작과 끝도 가능)
@@ -11,48 +12,59 @@
 
 // 문자열이 주어졌을 때, 팰린드롬으로 만들기 위해 필요한 연산의 최솟값을 출력하는 프로그램을 작성하시오.
 
-let input = 'ababacvabbab'.trim().split('')
+let input = 'nodong'.trim().split('')
 const len = input.length
 const half = Math.floor(len/2)
 let memo = Array(len-1).fill(-1)
-// console.log(memo)
-const jequi = (i,add,arry,count,diff,hc) =>
+const jequi = (num,i,add,arry,count,diff,hc) =>
 {
-    console.log(diff,arry[hc-i],arry[hc+i+add])
-    console.log(memo)
-    if(hc-i > 0 || i <= hc) 
+    if(i > hc)
     {
-        if(arry[hc-i] === arry[hc+i+add])
-        {
-            
-            [memo[hc-i],memo[hc+i+add]] = [1,1]
-            return jequi(i+1,add,arry,count,diff,hc)
-        }
-        [memo[hc-i],memo[hc+i+add]] = [arry[hc-i],arry[hc+i+add]]
-        diff.push([arry[hc-i],arry[hc+i+add]])
-        return jequi(i+1,add,arry,count+1,diff,hc)
+        memo[num] = count
+        return
     }
-    return diff
+    if(arry[num-i] === arry[num+i+add])
+    {
+        return jequi(num,i+1,add,arry,count,diff,hc)
+    }
+    diff.push([arry[num-i],arry[num+i+add]])
+    return jequi(num,i+1,add,arry,count+1,diff,hc)
 }
-const diffArry = len%2 === 0? jequi(0,1,input,0,[],half-1) : jequi(1,1,input,0,[],half-1)
-const sortArry = diffArry.map(e=> e.sort()).reduce((acc,cur,idx)=>
+for(let i = 1; i<input.length;i++)
 {
-    const i = acc[0].length -1
-    if(idx === 0)
+    if(input[i-1]=== input[i+1])
     {
-    acc[0].push(cur)
-    return acc
+        jequi(i,1,0,input,0,[],input.length-1)
     }
-    if(cur[0] === acc[0][i][0] && cur[1] === acc[0][i][1])
+    else if (input[i]  === input[i-1])
     {
-        acc[1] +=1 
-        return acc
+        jequi(i,0,1,input,0,[],input.length-1)
     }
-    acc[0].push(cur)
-    acc[1] +=1
-    return acc
+}
+memo = memo.filter(e => e> 0)
+console.log(Math.min.apply(null,memo))
 
-},[[],0])
-console.log(sortArry[1])
+
+// console.log(diffArry)
+// const sortArry = diffArry.map(e=> e.sort()).reduce((acc,cur,idx)=>
+// {
+
+//     const i = acc[0].length -1
+//     if(idx === 0)
+//     {
+//     acc[0].push(cur)
+//     return acc
+//     }
+//     if( cur[0] === acc[0][i][0] || cur[1] === acc[0][i][1])
+//     {
+//         acc[2] +=1 
+//         return acc
+//     }
+//     acc[0].push(cur)
+//     acc[1] +=1
+//     return acc
+
+// },[[],0,0])
+// console.log(sortArry)
 // const flatDiff = diffArry.flatMap(e => e).sort()
 // console.log(flatDiff)
